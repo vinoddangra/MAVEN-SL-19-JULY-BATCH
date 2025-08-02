e5fabbd1d5b3b44799d81ea20397c550bff807b3
Hitesh Kumar Sharma <hkshitesh@gmail.com>
2025-08-02 21:04:58 +0530

    div()added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -14,6 +14,10 @@ public class MyCalc {
 	{
 		return a*b;
 	}
+	public int div(int a, int b)
+	{
+		return a/b;
+	}
 
 	public static void main(String[] args) {
 
@@ -21,6 +25,7 @@ public class MyCalc {
 		System.out.println("Sum is :"+calc.sum(20, 10));
 		System.out.println("Diff is :"+calc.diff(20, 10));
 		System.out.println("mul is :"+calc.mul(20, 10));
+		System.out.println("Div is :"+calc.div(20, 10));
 	}
 
 }
